8affba1a77418ab76f5fa55c1f9bd935cdc3f3a0
Abraham Peter <abraham.peter.hash@gmail.com>
2023-03-21 14:58:45 +0400

    working on the header

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-red-400">Home</h1>;
+  return (
+    <>
+      <Header />
+      <h1 className="text-red-400">Home</h1>
+    </>
+  );
 }
