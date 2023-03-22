cfbf1fa9f2bd586e374dc21fbdec7c88ce3d050b
Abraham Peter <abraham.peter.hash@gmail.com>
2023-03-22 12:14:04 +0400

    first api call

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,28 @@
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
+const API_KEY = process.env.API_KEY;
+import Results from "@/components/Result";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+  console.log(results);
   return (
-    <>
-      <h1 className="text-red-400">Home</h1>
-    </>
+    <div>
+      <Results results={results} />
+    </div>
   );
 }
